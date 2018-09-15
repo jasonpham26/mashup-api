66081e5638e398bd4630303294473b571f1628dd
Phuong Pham <vinhphuong0206@gmail.com>
2018-09-15 14:04:23 +1000

    Structure updated

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
-const request = require("request");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const path = require("path");
 const mongoose = require("mongoose");
 const passport = require("passport");
 // Load User model
@@ -25,17 +28,48 @@ mongoose
 
 // Initial app
 const app = express();
-// MIDDLEWARES
+// MIDDLEWARES ---------------------------------------
+// Handlebars middleware
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+// Cookie parser
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false
+  })
+);
 // passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+// ---------------------------------------------------
+// GLOBAL VARIABELS
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
+});
 
-// ROUTES
+// PUBLIC FOLDER SETUP (folder to store css files)
+app.use(express.static(path.join(__dirname, "public")));
+// ROUTES --------------------------------------------
 // load routes
 const auth = require("./routes/auth");
+const spotify = require("./routes/spotify");
+const index = require("./routes/index");
 
+// ---------------------------------------------------
 // Use routes
+app.use("/", index);
 app.use("/auth", auth);
+app.use("/spotify", auth);
 
 // Port number
 const port = process.env.PORT || 5000;
